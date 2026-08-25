cdbbd362d086b7eac65f97210f00969193dcf709
gajendra <pahaladalodhi466@gmail.com>
2026-08-25 19:19:16 +0530

    Added Student class

---
+++ src/firstproject/Student.java
@@ -0,0 +1,14 @@
+package firstproject;
+
+public class Student {
+
+    public void display() {
+        System.out.println("My name is Gajendra");
+        System.out.println("I am learning Java Full Stack");
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.display();
+    }
+}
