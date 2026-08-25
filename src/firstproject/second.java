533364ebedee4af77cda7f22253b8a65e7fc3baf
gajendra <pahaladalodhi466@gmail.com>
2026-08-25 19:13:59 +0530

    Updated second Java class

--- src/firstproject/second.java
+++ src/firstproject/second.java
@@ -3,9 +3,7 @@ package firstproject;
 public class second {
 
 	public static void main(String[] args) {
-		
-		System.out.println("my name is Gajendra ");
-		System.out.println("i am from Damoh");
+		System.out.println("Hello GitHub - Java Developer");
 
 	}
 
