1ae76dd4935a7897140dc0803a757b73410446b9
gajendra <pahaladalodhi466@gmail.com>
2026-08-25 19:07:16 +0530

    Added second Java class

---
+++ src/firstproject/second.java
@@ -0,0 +1,12 @@
+package firstproject;
+
+public class second {
+
+	public static void main(String[] args) {
+		
+		System.out.println("my name is Gajendra ");
+		System.out.println("i am from Damoh");
+
+	}
+
+}
